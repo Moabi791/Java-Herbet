634a9e6dbbab91fc94eadadecf270389e932edc1
Moabi <moabilitsoane@gmail.com>
2021-10-12 14:47:46 +0200

    21th commit

---
+++ src/main/java/Chapter4/ChkNum.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter4;
+
+/**
+ *
+ * @author moabi
+ */
+class ChkNum {
+    
+        // return true if x is even 
+
+    boolean isEven(int x) {
+        if ((x % 2) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/Chapter4/Factor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter4;
+
+/**
+ *
+ * @author moabi
+ */
+public class Factor {
+    
+        boolean isFactor(int a, int b) {
+        if ((b % a) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
