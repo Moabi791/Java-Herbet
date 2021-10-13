78050b29acc85eca3e92fd03a9eb9e439db4653c
Moabi <moabilitsoane@gmail.com>
2021-10-13 13:31:09 +0200

    24th commit

---
+++ src/main/java/Chapter8/A.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter8;
+
+/**
+ *
+ * @author moabi
+ */
+
+public interface A {
+    void meth1(); 
+  void meth2();
+}
---
+++ src/main/java/Chapter8/Series.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter8;
+
+/**
+ *
+ * @author moabi
+ */
+public interface Series { 
+  int getNext(); // return next number in series 
+  void reset(); // restart 
+  void setStart(int x); // set starting value 
+}
+    
+
