cb80bbf26942d7c576da1f0c029a1cd2b91c97dd
Moabi <moabilitsoane@gmail.com>
2021-10-13 11:04:20 +0200

    23th commit

---
+++ src/main/java/Chapter6/VarArgs4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter6;
+
+/**
+ *
+ * @author moabi
+ */
+public class VarArgs4 {
+        // Use an int vararg parameter.  
+
+    static void vaTest(int... v) {
+        // ... 
+    }
+
+    // Use a boolean vararg parameter.  
+    static void vaTest(boolean... v) {
+        // ... 
+    }
+
+    public static void main(String args[]) {
+        vaTest(1, 2, 3);  // OK  
+        vaTest(true, false, false); // OK  
+
+        //vaTest(); // Error: Ambiguous! 
+        
+// Varargs, overloading, and ambiguity.  
+//  
+// This program contains an error and will  
+// not compile! 
+    }
+    
+}
