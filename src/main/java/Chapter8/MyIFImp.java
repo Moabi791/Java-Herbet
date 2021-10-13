9d2cb58a5c8a6a286ed5bed11439eceaa21b35b5
Moabi <moabilitsoane@gmail.com>
2021-10-13 14:18:24 +0200

    25th commit

---
+++ src/main/java/Chapter8/MyIFImp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter8;
+
+/**
+ *
+ * @author moabi
+ */
+public class MyIFImp implements MyIF {
+     // Only getUserID() defined by MyIF needs to be implemented. 
+  // getAdminID() can be allowed to default. 
+  public int getUserID() { 
+    return 100; 
+  } 
+    
+}
