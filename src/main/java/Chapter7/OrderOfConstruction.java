870ec479ae99ce7bfcdded9b6359e98da161e298
Moabi <moabilitsoane@gmail.com>
2021-10-13 11:48:01 +0200

    commit message

---
+++ src/main/java/Chapter7/OrderOfConstruction.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter7;
+
+/**
+ *
+ * @author moabi
+ */
+public class OrderOfConstruction {
+    
+        public static void main(String args[]) {
+        C c = new C();
+    }
+}
